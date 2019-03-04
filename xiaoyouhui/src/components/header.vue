9b7aeff6fc7c94f53755e8d424aec0f132d4c2c0
qin <1123276425@qq.com>
2019-03-04 15:08:58 +0800

    header

---
+++ xiaoyouhui/src/components/header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header id="header">
+        <div class="logo">
+            <img src="http://www.jituwang.com/uploads/allimg/151208/258057-15120R3593535.jpg" alt="">
+            <div></div>
+        </div>
+    </header>    
+</template>
+<script>
+    export default {
+        
+    }
+</script>
+<style lang="less" scoped>
+    #header{
+        .logo{
+            
+        }
+    }
+</style>
