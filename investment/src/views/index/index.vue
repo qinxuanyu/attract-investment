c8bf8234bd6a534f06830431a1e34b647393e59d
qin <1123276425@qq.com>
2019-03-05 11:11:32 +0800

    index页面

--- investment/src/views/index/index.vue
+++ investment/src/views/index/index.vue
@@ -4,14 +4,144 @@
       <img src="../../assets/trumpet.png" alt="" class="float-left clearfix"> 2019年我盟As的阿打算都是该款新车注册，共同促进大城市发展!
     </div>
     <div class="zhong">
-      <img src="../../assets/activity1.png" alt="" class="yi">
+      <img :src="currimg" alt="" class="yi" :class="{'active':dh}">
       <div class="er">
-        <img :src="item" alt="" v-for="(item, index) in imglist" :key="index" class="potor">
+        <img :src="item" alt="" :class="{'active':index==curr}" v-for="(item, index) in imglist" :key="index" class="potor" @mouseenter="jing(index)">
       </div>
-      <div class="san"></div>
+      <div class="san">
+        <div class="li">
+          <div class="Journalism">
+            <span class="span1">招引新闻</span>
+            <span class="span2">更多</span>
+          </div>
+          <div class="p gg">驾驶的烦恼啊大大群发萨达所大所大所多动奥术大师大所多,机是的分公司的过分发给</div>
+          <div class="p g">驾驶的烦恼啊As的大大缩短都是发鬼地方个大概a阿斯达四大大范甘迪郭德纲沃尔沃若翁人徐晓晨</div>
+          <div class="p">驾驶的烦恼啊大大群发萨给</div>
+          <div class="p">驾驶的烦恼师大所多,机是的分公司的过分发给</div>
+          <div class="p">驾驶的烦恼啊大大群发萨给</div>
+          <div class="p">驾驶的烦恼师大所多,机是的分公司的过分发给</div>
+          <div class="p">驾驶的烦恼啊大大群发萨给</div>
+          <div class="p">驾驶的烦恼师大所多,机是的分公司的过分发给</div>
+          <div class="p">驾驶的烦恼啊大大群发萨给</div>
+          <div class="p">驾驶的烦恼师大所多,机是的分公司的过分发给</div>
+        </div>
+      </div>
     </div>
+    <img src="../../assets/advertising1.png" alt="">
+    <!--第一个-->
+    <div class="xia">
+      <div class="z">
+        <div class="Journalism">
+          <span class="span1">招商活动</span>
+          <span class="span2">更多</span>
+        </div>
+        <div class="flex">
+          <div class="lm">
+            <img src="../../assets/activity1.png" alt="">
+            <h3 class="h3">首届阿联酋(2019)中国品牌博览会</h3>
+          </div>
+          <div class="lm">
+            <img src="../../assets/activity1.png" alt="">
+            <h3 class="h3">首届阿联酋(2019)中国品牌博览会</h3>
+          </div>
+          <div class="lm">
+            <img src="../../assets/activity1.png" alt="">
+            <h3 class="h3">首届阿联酋(2019)中国品牌博览会</h3>
+          </div>
+          <div class="lm">
+            <img src="../../assets/activity1.png" alt="">
+            <h3 class="h3">首届阿联酋(2019)中国品牌博览会</h3>
+          </div>
+          <div class="lm">
+            <img src="../../assets/activity1.png" alt="">
+            <h3 class="h3">首届阿联酋(2019)中国品牌博览会</h3>
+          </div>
+          <div class="lm">
+            <img src="../../assets/activity1.png" alt="">
+            <h3 class="h3">首届阿联酋(2019)中国品牌博览会</h3>
+          </div>
 
+        </div>
+      </div>
+      <div class="y">
+        <div class="yy">
+          <div class="Journalism">
+            <span class="span1" style="color:#001F84">招引服务</span>
+            <span class="span2">更多</span>
+          </div>
+          <div class="p gg">驾驶的烦恼啊大大群发萨给</div>
+          <div class="p">驾驶的烦恼师大所多,机是的分公司的过分发给</div>
+          <div class="p">驾驶的烦恼啊大大群发萨给</div>
+          <div class="p">驾驶的烦恼啊大大群发萨给</div>
+          <div class="p">驾驶的烦恼师大所多,机是的分公司的过分发给</div>
+          <div class="p">驾驶的烦恼啊大大群发萨给</div>
+          <div class="p">驾驶的烦恼啊大大群发萨给</div>
+          <div class="p">驾驶的烦恼师大所多,机是的分公司的过分发给</div>
+          <div class="p">驾驶的烦恼啊大大群发萨给</div>
+        </div>
+      </div>
 
+    </div>
+    <!--第二个-->
+    <div class="xia">
+      <div class="z">
+        <div class="Journalism">
+          <span class="span1">建党工作</span>
+          <span class="span2">更多</span>
+        </div>
+        <div class="flex">
+          <div class="zf">
+            <h3 style="text-align: left;padding-bottom: 5px;">商务局2019年建党工作要点</h3>
+            <span class="span">事务局关于开展'党建模范建设'</span>
+            <span class="span">事务局关于开展'党建模范建设'</span>
+            <span class="span">事务局关于开展'党建模范建设'</span>
+            <span class="span">事务局关于开展'党建模范建设'</span>
+            <span class="span">事务局关于开展'党建模范建设'</span>
+            <span class="span">事务局关于开展'党建模范建设'</span>
+            <span class="span">事务局关于开展'党建模范建设'</span>
+          </div>
+          <div class="yf">
+            <div class="lm">
+              <img src="../../assets/activity1.png" alt="">
+              <h3 class="h3">首届阿联酋(2019)中国品牌博览会</h3>
+            </div>
+            <div class="lm">
+              <img src="../../assets/activity1.png" alt="">
+              <h3 class="h3">首届阿联酋(2019)中国品牌博览会</h3>
+            </div>
+            <div class="lm">
+              <img src="../../assets/activity1.png" alt="">
+              <h3 class="h3">首届阿联酋(2019)中国品牌博览会</h3>
+            </div>
+
+            <div class="lm">
+              <img src="../../assets/activity1.png" alt="">
+              <h3 class="h3">首届阿联酋(2019)中国品牌博览会</h3>
+            </div>
+
+          </div>
+        </div>
+      </div>
+      <div class="y">
+        <div class="yy">
+          <div class="Journalism">
+            <span class="span1" style="color:#001F84">三圈动态</span>
+            <span class="span2">更多</span>
+          </div>
+          <div class="p gg">驾驶的烦恼啊大大群发萨给</div>
+          <div class="p">驾驶的烦恼师大所多,机是的分公司的过分发给</div>
+          <div class="p">驾驶的烦恼啊大大群发萨给</div>
+          <div class="p">驾驶的烦恼啊大大群发萨给</div>
+          <div class="p">驾驶的烦恼师大所多,机是的分公司的过分发给</div>
+          <div class="p">驾驶的烦恼啊大大群发萨给</div>
+          <div class="p">驾驶的烦恼啊大大群发萨给</div>
+          <div class="p">驾驶的烦恼师大所多,机是的分公司的过分发给</div>
+          <div class="p">驾驶的烦恼啊大大群发萨给</div>
+        </div>
+      </div>
+
+    </div>
+    <img src="../../assets/advertising2.png" alt="" class="hh">
   </div>
 </template>
 <script>
@@ -19,11 +149,22 @@
 
     data() {
       return {
-       imglist:[require('../../assets/activity2.png'),require('../../assets/activity2.png'),require('../../assets/activity2.png'),require('../../assets/activity2.png'),]
+        imglist: [require('../../assets/activity2.png'), require('../../assets/activity3.png'), require(
+          '../../assets/activity4.png'), require('../../assets/activity2.png'), ],
+        curr: 0,
+        currimg: require('../../assets/activity4.png'),
+        dh: false
       }
     },
     methods: {
-
+      jing(index) {
+        this.curr = index
+        this.currimg = this.imglist[index]
+        this.dh = true
+        setTimeout(() => {
+          this.dh = false
+        }, 200);
+      }
     }
 
   }
@@ -66,10 +207,12 @@
   .zhong {
     height: 490px;
     display: flex;
+    margin-bottom: 35px;
   }
 
   .yi {
     flex: 2;
+    transition: 500ms;
   }
 
   .er {
@@ -81,13 +224,157 @@
   .san {
     flex: 3;
   }
-  .er:nth-child(2){
 
-  margin:0 5px 0px 5px;
+  .potor:nth-child(1) {
+    margin: 0 5px 3px 5px;
   }
-.potor{
-  flex: 1;
-  height: 115px;
-  width: 200px;
+
+  .potor:nth-child(2) {
+    margin: 3px 5px 3px 5px;
   }
+
+  .potor:nth-child(3) {
+    margin: 3px 5px 3px 5px;
+  }
+
+  .potor:nth-child(4) {
+    margin: 3px 5px 0px 5px;
+  }
+
+  .potor {
+    flex: 1;
+    height: 115px;
+    width: 200px;
+  }
+
+  .potor.active {
+    border: 3px red solid;
+  }
+
+  .yi.active {
+    opacity: 0.5;
+  }
+
+  .Journalism {
+    height: 48px;
+    border-bottom: 1px #C71106 solid;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .span1 {
+    color: red;
+    font-size: 22px;
+  }
+
+  .span2 {
+    color: #828282;
+    font-size: 19px;
+  }
+
+  .li {
+    padding: 0 23px;
+    text-align: left;
+  }
+
+  .p {
+    font-size: 17px;
+    color: #2D2D2D;
+    line-height: 28px;
+    margin: 10px 0;
+  }
+
+  .g {
+    font-size: 15px;
+    color: #919191;
+  }
+
+  .xia {
+    display: flex;
+    height: 470px;
+    margin-top: 25px;
+  }
+
+  .z {
+    flex: 3;
+  }
+
+  .y {
+    flex: 2;
+  }
+
+  .flex {
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    height: calc(100% - 48px);
+    padding: 27px 0 3px 0;
+  }
+
+  .lm {
+    flex: 0 0 33.33%;
+    text-align: left;
+    /*margin: 23px 0 0px 0;*/
+  }
+
+  .flex .lm:nth-child(3n) {
+    text-align: right
+  }
+
+  .flex .lm:nth-child(2) {
+    text-align: center
+  }
+
+  .flex .lm:nth-child(5) {
+    text-align: center;
+  }
+
+  .h3 {
+    font-size: 15px;
+    padding: 8px 0;
+    font-weight: normal;
+  }
+
+  .gg {
+    margin: 20px 0 10px 0;
+  }
+
+  .yy {
+    padding: 0 29px;
+    text-align: left;
+    color: #303030;
+  }
+
+  .zf {
+    flex: 1;
+  }
+
+  .yf {
+    flex: 2;
+  }
+
+  .span {
+    display: block;
+    text-align: left;
+    padding: 11px 0;
+    color: #808080;
+  }
+
+  .yf {
+    display: flex;
+    flex-wrap: wrap;
+    padding-left: 20px;
+  }
+
+  .flex .yf .lm {
+    flex: 0 0 50%;
+    text-align: left;
+  }
+
+  .flex .yf .lm:nth-child(2n) {
+    text-align: right;
+  }
+.hh{
+      margin: 20px 0 60px 0;
+}
 </style>
